d754eaa60b8943fc9962136d321f43f06e72e26b
Park Hyunwoo <ez.amiryo@gmail.com>
2019-07-12 17:42:48 +0900

    Fix Firebase issues

--- src/site/Firebase/firebase.js
+++ src/site/Firebase/firebase.js
@@ -2,18 +2,9 @@ import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-};
-
 class Firebase {
   constructor() {
-    app.initializeApp(config);
+    app.initializeApp(JSON.parse(process.env.FIREBASE_CONFIG));
 
     this.auth = app.auth();
     this.db = app.firestore();

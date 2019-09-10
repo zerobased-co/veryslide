947e31cb6af988d773f7fa08ece75b44721b1935
Park Hyunwoo <ez.amiryo@gmail.com>
2019-09-10 15:32:51 +0900

    Fix ColorButton for ambiguous value

--- src/components/ui/ColorButton.js
+++ src/components/ui/ColorButton.js
@@ -27,11 +27,13 @@ class ColorButton extends View {
   }
 
   showPicker(event) {
+    const color = (this.color === '?') ? '#FFFFFF' : this.color;
+
     this.colorPicker.show({
       left: event.clientX - 100,
       top: event.clientY + 14,
       hideDelay: 2000,
-    }, this.color, (newColor) => {
+    }, color, (newColor) => {
       this.color = newColor;
       this.onChange(newColor);
     });
@@ -40,7 +42,7 @@ class ColorButton extends View {
   on_color(color) {
     this.node.style.backgroundColor = color;
     this.node.innerText = color.toUpperCase();
-    this.node.style.color = properTextColor(color);
+    this.node.style.color = (this.color === '?') ? '#000000' : properTextColor(color);
   }
 }
 

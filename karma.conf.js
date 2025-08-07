ebf5626de5171b40833790bc1f04823d4d542937
Park Hyunwoo <ez.amiryo@gmail.com>
2025-08-07 21:11:22 +0900

    add launch.json for vscode

--- karma.conf.js
+++ karma.conf.js
@@ -1,30 +1,21 @@
 var webpackConfig = require('./webpack.config.js');
 
 module.exports = function(config) {
+  const grepPattern = config.grep || process.argv.find(arg => arg.startsWith('--grep='))?.split('=')[1] || '';
+  
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha', 'webpack'],
 
+    client: {
+      mocha: {
+        grep: grepPattern,
+      }
+    },
 
-    // list of files / patterns to load in the browser
     files: [
       'test/index.js',
     ],
-
-
-    // list of files / patterns to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'test/index.js': ['webpack', 'sourcemap'],
     },
@@ -34,10 +25,6 @@ module.exports = function(config) {
       stats: 'errors-only',
     },
 
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['dots', 'coverage'],
 
     coverageReporter: {
@@ -47,36 +34,22 @@ module.exports = function(config) {
       ],
     },
 
-
-    // web server port
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['ChromeHeadless'],
     browserNoActivityTimeout: 60000,
 
+    customLaunchers: {
+      ChromeDebugging: {
+        base: 'Chrome',
+        flags: ['--remote-debugging-port=9333']
+      }
+    },
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
     concurrency: Infinity
   })
 }
